fc13bcfe93b894843c99c690306701a0641267d2
mdimransorker <apgimran@gmail.com>
2022-05-30 01:33:04 +0600

    add

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${DB_USER}:${DB_PAS}@cluster0.mphb1.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PAS}@cluster0.mphb1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+console.log(uri);
 
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
